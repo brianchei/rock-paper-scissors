b1d4f0eb39319cfd0e656191a9cabed8c5c02f6d
Brian Chei <157544112+brianchei@users.noreply.github.com>
2024-08-09 17:47:51 -0500

    feat: create index & script, add pseudocode
    Complete html boilerplate for homepage, pseudocode for main js.

---
+++ main.js
@@ -0,0 +1,70 @@
+/*
+Pseudocode
+Declare play game function
+    Declare play round function
+    Declare/initialize score variables
+    Calculate random computer choice ||
+    Ask user for input
+    If rock
+        Log/print choice
+        Compare choices ||
+        Player win?
+            Increment player score
+            Log winner announcement
+            Else increment computer score
+            Log winner announcement
+    If paper
+        Log/print choice
+        Compare choices ||
+        Player win?
+            Increment player score
+            Log winner announcement
+            Else increment computer score
+            Log winner announcement
+    If scissors
+        Log/print choice
+        Compare choices ||
+        Player win?
+            Increment player score
+            Log winner announcement
+            Else increment computer score
+            Log winner announcement
+
+
+Calculate random computer choice
+Start
+Generate random number 0-2
+If 0
+    Set computer choice rock
+If 1
+    Set computer choice paper
+If 2
+    Set computer choice scissors
+
+
+Compare choices
+  Start
+  If player choice equal computer choice
+    Tie announcement
+  Else if player choice rock
+    If computer choice equal paper
+        Increment computer score
+        Winner announcement
+    Else
+        Increment player score
+        Winner announcement
+  Else if player choice paper
+    If computer choice equal scissors
+        Increment computer score
+        Winner announcement
+    Else
+        Increment player score
+        Winner announcement
+  Else if player choice scissors
+    If computer choice equal rock
+        Increment computer score
+        Winner announcement
+    Else
+        Increment player score
+        Winner announcement
+*/
