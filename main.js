9da1adf0d208b231f3735191836ca0a7014f2636
Brian Chei <157544112+brianchei@users.noreply.github.com>
2024-08-09 17:49:35 -0500

    feat: add comp. choice, randomized

--- main.js
+++ main.js
@@ -68,3 +68,15 @@ Compare choices
         Increment player score
         Winner announcement
 */
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    console.log(choice);
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
